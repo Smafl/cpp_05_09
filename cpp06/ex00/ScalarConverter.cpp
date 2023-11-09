04018d9e60f8c797c3ef2e6d7001969883cbe8bd
Lena <lennykulichkova@gmail.com>
2023-11-09 23:35:33 +0100

    cpp06/ex02 in process

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -1,21 +1,42 @@
 
 #include "ScalarConverter.hpp"
+#include <iostream>
+#include <cctype>
+
+namespace {
+enum Type {
+	CHARACTER,
+	INTEGER,
+	FLOAT,
+	DOUBLE,
+	NONE,
+};
+
+Type getType(const std::string &str) {
+	size_t pos(str.find("."));
+	double d;
+	if (str.size() == 1 && !std::isdigit(str[0]))
+		return CHARACTER;
+	else if (pos != std::string::npos) {
+		
+	}
+
+}
+}
 
 ScalarConverter::ScalarConverter() { }
 
-ScalarConverter::ScalarConverter(const ScalarConverter &other) {
-	(void)other;
-}
+ScalarConverter::ScalarConverter(const ScalarConverter &) { }
 
 ScalarConverter::~ScalarConverter() { }
 
-ScalarConverter &ScalarConverter::operator=(const ScalarConverter &other) {
-	(void)other;
+ScalarConverter &ScalarConverter::operator=(const ScalarConverter &) {
 	return *this;
 }
 
 void ScalarConverter::convert(const std::string &str) {
-	// parse input, detect a type
+	Type t(getType(str));
+
 	// convert it explicitly
 	// display: char, int, float, double
 }
