2bd6e2dae73a05176c162e438c58fcede658dcbc
Elena Kulichkova <ekulichk@1-A-32.42heilbronn.de>
2023-11-18 17:35:32 +0100

    finish cpp06/ex00

--- cpp06/ex00/Converter.cpp
+++ cpp06/ex00/Converter.cpp
@@ -14,53 +14,9 @@ enum Type {
 	INTEGER,
 	FLOAT,
 	DOUBLE,
-	NONE,
-	POSINF,
-	NEGINF,
 	OVER,
 	ERROR
 };
-
-// Type getType(char *str) {
-// 	std::size_t len = std::strlen(str);
-// 	std::string sstr(str);
-// 	std::size_t dot_pos = sstr.find(".");
-
-// 	if (len == 1 && !std::isdigit(str[0]))
-// 		return CHARACTER;
-
-// 	errno = 0;
-// 	char *endpnt = NULL;
-// 	double dvalue = std::strtod(str, &endpnt);
-// 	if (errno != 0)
-// 		return OVER;
-// 	// std::cout << errno << std::endl;
-// 	if (*endpnt == '\0') {
-// 		if (dvalue == DBL_MAX)
-// 			return POSINF;
-// 		else if (dvalue == -DBL_MAX)
-// 			return NEGINF;
-// 		else if (isinf(dvalue)) {
-// 			if (sstr.find("-") != std::string::npos)
-// 				return NEGINF;
-// 			return POSINF;
-// 		}
-// 		else if (isnan(dvalue))
-// 			return NONE;
-// 		else if ((dvalue >= std::numeric_limits<int>::min() || dvalue <= std::numeric_limits<int>::max()) &&
-// 				dot_pos == std::string::npos)
-// 			return INTEGER;
-// 		else if (dvalue == 0.0 && str != endpnt) {
-// 			return ERROR;
-// 		}
-// 		else
-// 			return DOUBLE;
-// 	}
-// 	else if (str[len - 1] == 'f' && dot_pos != std::string::npos &&
-// 		(dvalue >= std::numeric_limits<float>::min() || dvalue <= std::numeric_limits<float>::max()))
-// 		return FLOAT;
-// 	return ERROR;
-// }
 
 bool isFloat(double dvalue) {
 
@@ -69,6 +25,13 @@ bool isFloat(double dvalue) {
 	if ((dvalue >= std::numeric_limits<float>::min() || dvalue <= std::numeric_limits<float>::max())) {
 		return true;
 	}
+	return false;
+}
+
+bool isInt(double dvalue, std::size_t dot_pos) {
+	if (dot_pos == std::string::npos &&
+		(dvalue >= std::numeric_limits<int>::min() && dvalue <= std::numeric_limits<int>::max()))
+		return true;
 	return false;
 }
 
@@ -84,11 +47,12 @@ Type getType(char *str) {
 	std::string sstr(str);
 	std::size_t dot_pos = sstr.find(".");
 	if (*endpnt == '\0') {
-		if (dot_pos == std::string::npos &&
-			(dvalue >= std::numeric_limits<int>::min() || dvalue <= std::numeric_limits<int>::max()))
+		if (isInt(dvalue, dot_pos))
 			return INTEGER;
 		else if (isnan(dvalue) || isinf(dvalue))
 			return DOUBLE;
+		else if (dot_pos == std::string::npos)
+			return ERROR;
 		else
 			return DOUBLE;
 	}
@@ -97,40 +61,44 @@ Type getType(char *str) {
 	return ERROR;
 }
 
-void printPseudo(std::string str) {
-	std::cout << "char: impossible" << std::endl;
-	std::cout << "int: impossible" << std::endl;
-	std::cout << "float: " << str << "f" << std::endl;
-	std::cout << "double: " << str << std::endl;
+void printChar(double dvalue) {
+	if (dvalue >= std::numeric_limits<char>::min() && dvalue <= std::numeric_limits<char>::max()) {
+		if (std::isprint(static_cast<char>(dvalue)))
+			std::cout << "char: '" << static_cast<char>(dvalue) << "'" << std::endl;
+		else
+			std::cout << "char: Non displayable" << std::endl;
+	}
+	else
+		std::cout << "char: impossible" << std::endl;
 }
 
-void printChar(char c) {
+void charConvert(char c) {
 	if (std::isprint(c))
 		std::cout << "char: '" << c << "'" << std::endl;
 	else
 		std::cout << "char: Non displayable" << std::endl;
-}
-
-void charConvert(char c) {
-	printChar(c);
 	std::cout << "int: " << static_cast<int>(c) << std::endl;
 	std::cout << std::fixed << std::setprecision(1) << "float: " << static_cast<float>(c) << "f" << std::endl;
 	std::cout << std::fixed << std::setprecision(1) << "double: " << static_cast<double>(c) << std::endl;
 }
 
 void intConvert(char *str) {
-	int ivalue(std::atoi(str));
-	printChar(static_cast<char>(ivalue));
-	std::cout << "int: " << ivalue << std::endl;
-	std::cout << std::fixed << std::setprecision(1) << "float: " << static_cast<float>(ivalue) << "f" << std::endl;
-	std::cout << std::fixed << std::setprecision(1) << "double: " << static_cast<double>(ivalue) << std::endl;
+	char *endpnt = NULL;
+	double dvalue = std::strtod(str, &endpnt);
+	printChar(dvalue);
+	std::cout << "int: " << static_cast<int>(dvalue) << std::endl;
+	std::cout << std::fixed << std::setprecision(1) << "float: " << static_cast<float>(dvalue) << "f" << std::endl;
+	std::cout << std::fixed << std::setprecision(1) << "double: " << static_cast<double>(dvalue) << std::endl;
 }
 
 void floatConvert(char *str) {
 	char *endpnt = NULL;
 	double dvalue = std::strtod(str, &endpnt);
-	printChar(static_cast<char>(dvalue));
-	std::cout << "int: " << static_cast<int>(dvalue) << std::endl;
+	printChar(dvalue);
+	if (dvalue >= std::numeric_limits<int>::min() && dvalue <= std::numeric_limits<int>::max())
+		std::cout << "int: " << static_cast<int>(dvalue) << std::endl;
+	else
+		std::cout << "int: impossible" << std::endl;
 	std::cout << std::fixed << std::setprecision(1) << "float: " << static_cast<float>(dvalue) << "f" << std::endl;
 	std::cout << std::fixed << std::setprecision(1) << "double: " << dvalue << std::endl;
 }
@@ -138,11 +106,26 @@ void floatConvert(char *str) {
 void doubleConvert(char *str) {
 	char *endpnt = NULL;
 	double dvalue = std::strtod(str, &endpnt);
-	printChar(static_cast<char>(dvalue));
-	std::cout << "int: " << static_cast<int>(dvalue) << std::endl;
-	std::cout << std::fixed << std::setprecision(1) << "float: " << static_cast<float>(dvalue) << "f" << std::endl;
+	printChar(dvalue);
+	if (dvalue >= std::numeric_limits<int>::min() && dvalue <= std::numeric_limits<int>::max())
+		std::cout << "int: " << static_cast<int>(dvalue) << std::endl;
+	else
+		std::cout << "int: impossible" << std::endl;
+	if (dvalue >= std::numeric_limits<float>::min() || dvalue <= std::numeric_limits<float>::max())
+		std::cout << std::fixed << std::setprecision(1) << "float: " << static_cast<float>(dvalue) << "f" << std::endl;
+	else
+		std::cout << "float: impossible" << std::endl;
 	std::cout << std::fixed << std::setprecision(1) << "double: " << dvalue << std::endl;
 }
+}
+
+void printError() {
+	std::cout << "impossible to detect a type" << std::endl;
+	std::cout << "input examples:" << std::endl;
+	std::cout << "for char: c, a, ..." << std::endl;
+	std::cout << "for int: 0, -42, 42, ..." << std::endl;
+	std::cout << "for float: 0.0f, -4.2f, 4.2f, ..." << std::endl;
+	std::cout << "for double: 0.0, -4.2, 4.2, ..." << std::endl;
 }
 
 // CONVERTION
@@ -171,11 +154,7 @@ void ScalarConverter::convert(char *str) {
 		std::cout << "out of range" << std::endl;
 		break;
 	case ERROR:
-		std::cout << "impossible to detect a type" << std::endl;
+		printError();
 		break;
 	}
 }
-
-// todo:
-// redo all convert function and
-// check nan, +/-inf there
