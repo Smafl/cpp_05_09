b35c10e48b1fc11b02e50120d01ae7cec86fa91a
Elena Kulichkova <ekulichk@1-A-32.42heilbronn.de>
2023-12-01 17:16:18 +0100

    finish cpp08/ex02

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -6,17 +6,50 @@
 
 template <class T>
 class MutantStack : public std::stack<T> {
-private:
-    std::stack<T> _stack;
 public:
-    MutantStack();
-    MutantStack(const MutantStack &other);
-    ~MutantStack();
+    MutantStack() { }
+    MutantStack(const MutantStack &) { }
+    ~MutantStack() { }
 
-    MutantStack &operator=(const MutantStack &other);
+    MutantStack &operator=(const MutantStack &other) {
+		if (*this != other)
+			this->c = other.c;
+		return *this;
+	}
 
+//	iterators
+	typename std::stack<T>::container_type::iterator it_begin() {
+		return std::stack<T>::c.begin();
+	}
+
+	typename std::stack<T>::container_type::iterator it_end() {
+		return std::stack<T>::c.end();
+	}
+
+	typename std::stack<T>::container_type::const_iterator it_begin_const() const {
+		return std::stack<T>::c.begin();
+	}
+
+	typename std::stack<T>::container_type::const_iterator it_end_const() const {
+		return std::stack<T>::c.end();
+	}
+
+//	reverse iterators
+	typename std::stack<T>::container_type::reverse_iterator it_rbegin() {
+		return std::stack<T>::c.rbegin();
+	}
+
+	typename std::stack<T>::container_type::reverse_iterator it_rend() {
+		return std::stack<T>::c.rend();
+	}
+
+	typename std::stack<T>::container_type::const_reverse_iterator it_rbegin_const() const {
+		return std::stack<T>::c.rbegin();
+	}
+
+	typename std::stack<T>::container_type::const_reverse_iterator it_rend_const() const {
+		return std::stack<T>::c.rend();
+	}
 };
-
-#include "MutantStack.tpp"
 
 #endif // MUTANTSTACK_HPP
--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -6,10 +6,40 @@
 int main()
 {
     MutantStack<int> msInt;
-    MutantStack<char> msChar;
     msInt.push(5);
     msInt.push(10);
     std::cout << "Top is " << msInt.top() << std::endl;
     std::cout << "Size is " << msInt.size() << std::endl;
+	for (int i = 0; i != 23; i++) {
+		msInt.push(i + 1);
+	}
+	std::cout << *msInt.it_begin() << std::endl;
+	for (MutantStack<int>::container_type::iterator it = msInt.it_begin(); it != msInt.it_end(); it++) {
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
+
+    // MutantStack<char> msChar;
+	// for (int i = 0; i != 23; i++) {
+	// 	msChar.push(i + 55);
+	// }
+	// for (MutantStack<char>::container_type::iterator it = msChar.it_begin(); it != msChar.it_end(); it++) {
+	// 	std::cout << *it << " ";
+	// }
+	// std::cout << std::endl;
+	// for (MutantStack<char>::container_type::reverse_iterator rit = msChar.it_rbegin(); rit != msChar.it_rend(); rit++) {
+	// 	std::cout << *rit << " ";
+	// }
+	// std::cout << std::endl;
+	// MutantStack<char>::container_type::reverse_iterator rit = msChar.it_rbegin();
+	// *rit = 'x';
+	// for (MutantStack<char>::container_type::reverse_iterator rit = msChar.it_rbegin(); rit != msChar.it_rend(); rit++) {
+	// 	std::cout << *rit << " ";
+	// }
+	// std::cout << std::endl;
+
+	// MutantStack<char>::container_type::const_reverse_iterator rit_const = msChar.it_rend_const() - 1;
+	// std::cout << *rit_const << std::endl;
+	// *rit_const = 'x';
     return 0;
 }
