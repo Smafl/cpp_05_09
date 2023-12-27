f6205ebc5f161c7f3663f7f5144567a22a975bb7
Elena Kulichkova <ekulichk@1-E-23.42heilbronn.de>
2023-12-27 18:21:04 +0100

    add get_tk() cpp09/ex02

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -1,21 +1,24 @@
 
 #include "PmergeMe.hpp"
 #include <vector>
+#include <cmath>	// pow()
 
-void sort(std::vector<PairBase*> &input) {
+int get_tk(int k) {
+	return (std::pow(2, k + 1) + std::pow(-1, k)) / 3;
+}
+
+// this function is doing actual sorting
+void mi_sort(std::vector<PairBase*> &input) {
 	std::size_t n = input.size();
 	std::vector<PairBase*> pair;
 	for (std::size_t i = 0; i + 1 < n; i += 2) {
 		Pair *p = new Pair(input[i], input[i + 1]);
 		pair.push_back(p);
 	}
-	sort(pair);
+	mi_sort(pair);
 
 	std::vector<PairBase*> sorted;
-	std::vector<PairBase*> linked; // b1 b2 b3 ...
-	// b3 = linked[2]
-	// t1 = 1 -> 0
-	// t2 = 3 -> 2
+	std::vector<PairBase*> linked;
 	sorted.push_back(dynamic_cast<Pair*>(pair[0])->getMin());
 	for (std::size_t i = 0; i != pair.size(); i++) {
 		sorted.push_back(dynamic_cast<Pair*>(pair[i])->getMax());
@@ -24,8 +27,24 @@ void sort(std::vector<PairBase*> &input) {
 	}
 	if (n % 2 != 0)
 		linked.push_back(input[n - 1]);
+
+	int index;
+	int tk;
+	int tk2;
+
+	for (int k = 2; ; k++) {
+		tk = get_tk(k);
+		tk2 = get_tk(k - 1);
+		for (int i = tk; i != tk2; i--) {
+
+		}
+	}
 }
 
+// this function is called in main()
+// std::vector<PairBase*> is made from vector<int> from input
+// then sorting
+// then unwrapped std::vector<PairBase*> to vector<int> back
 void sort(std::vector<int> &input) {
 	std::vector<PairBase*> pairBase;
 	for (std::vector<int>::const_iterator it = input.begin(); it != input.end(); it++) {
@@ -33,17 +52,10 @@ void sort(std::vector<int> &input) {
 		pairBase.push_back(nbr);
 	}
 
-	sort(pairBase);
+	mi_sort(pairBase);
 
 	for (std::size_t i = 0; i != pairBase.size(); i++) {
 		input[i] = pairBase[i]->getNbr();
 		delete pairBase[i];
 	}
 }
-
-/*
-[ *     ][ *     ][ *     ]
-  |        |        |    
-  v        v        v    
-  Unpair   Unpair   Unpair
-*/
